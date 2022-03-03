0ca34b83a696ff5ae79537d4e4c61dcee7dd6790
Georges Ribeiro <georges@simplyinsured.com>
2022-03-03 02:07:19 -0300

    Roar!

---
+++ kong/plugins/tcp-log-body/handler.lua
@@ -0,0 +1,94 @@
+local cjson = require "cjson"
+local sandbox = require "kong.tools.sandbox".sandbox
+
+
+local kong = kong
+local ngx = ngx
+local timer_at = ngx.timer.at
+
+
+local sandbox_opts = { env = { kong = kong, ngx = ngx } }
+
+
+local function log(premature, conf, message)
+  if premature then
+    return
+  end
+
+  local host = conf.host
+  local port = conf.port
+  local timeout = conf.timeout
+  local keepalive = conf.keepalive
+
+  local sock = ngx.socket.tcp()
+  sock:settimeout(timeout)
+
+  local ok, err = sock:connect(host, port)
+  if not ok then
+    kong.log.err("failed to connect to ", host, ":", tostring(port), ": ", err)
+    return
+  end
+
+  if conf.tls then
+    ok, err = sock:sslhandshake(true, conf.tls_sni, false)
+    if not ok then
+      kong.log.err("failed to perform TLS handshake to ", host, ":", port, ": ", err)
+      return
+    end
+  end
+
+  ok, err = sock:send(cjson.encode(message) .. "\n")
+  if not ok then
+    kong.log.err("failed to send data to ", host, ":", tostring(port), ": ", err)
+  end
+
+  ok, err = sock:setkeepalive(keepalive)
+  if not ok then
+    kong.log.err("failed to keepalive to ", host, ":", tostring(port), ": ", err)
+    return
+  end
+end
+
+
+local TcpLogHandlerBody = {
+  PRIORITY = 7,
+  VERSION = "1.0.0",
+}
+
+function TcpLogHandlerBody:access(conf)
+    local ctx = kong.ctx.plugin;
+    ctx.request_body = kong.request.get_raw_body();
+end
+  
+  
+function TcpLogHandlerBody:body_filter(conf)
+    local ctx = kong.ctx.plugin;
+    local chunk, eof = ngx.arg[1], ngx.arg[2];
+    if not eof then
+        ctx.response_body = (ctx.response_body or "") .. (chunk or "")
+    end
+end
+
+
+function TcpLogHandlerBody:log(conf)
+  local ctx = kong.ctx.plugin;
+  local set_serialize_value = kong.log.set_serialize_value;
+  if conf.custom_fields_by_lua then
+    
+    for key, expression in pairs(conf.custom_fields_by_lua) do
+      set_serialize_value(key, sandbox(expression, sandbox_opts)())
+    end
+  end
+
+  set_serialize_value("request_body", ctx.request_body)
+  set_serialize_value("response_body", ctx.response_body)
+
+  local message = kong.log.serialize()
+  local ok, err = timer_at(0, log, conf, message)
+  if not ok then
+    kong.log.err("failed to create timer: ", err)
+  end
+end
+
+
+return TcpLogHandlerBody
